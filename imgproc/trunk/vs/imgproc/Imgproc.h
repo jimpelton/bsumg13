7a7fb836d0426b651ac7e91d370643776a66e206
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-01-21 07:34:58 +0000

    Added Imgproc.h

---
+++ imgproc/trunk/vs/imgproc/Imgproc.h
@@ -0,0 +1,22 @@
+/********************************************************************
+	filename: 	E:\Programming\microgravity\imgproc.svn\trunk\imgproc\imgproc\Imgproc.h
+	file base:	Imgproc
+	author:		jim
+	
+	purpose:	The main Imgproc interface for client applications.
+*********************************************************************/
+
+
+#ifndef Imgproc_h__
+#define Imgproc_h__
+/**
+ *	\brief Provides an interface to the Imgproc system for client applications.
+ */
+class Imgproc
+{
+    //TODO: write me!
+
+
+};
+
+#endif // Imgproc_h__
