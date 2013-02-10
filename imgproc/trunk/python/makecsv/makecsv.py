79749154471ee68bcd848ce9d6f5daafc7f74293
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-02-10 07:59:03 +0000

    Cleaned up makecsv.py, simplified file reading, added part of concentration calculations.

--- imgproc/trunk/python/makecsv/makecsv.py
+++ imgproc/trunk/python/makecsv/makecsv.py
@@ -8,38 +8,40 @@ from math import sqrt
 #
 def getArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d485", "--Directory485", help="485 Images Directory")
-    parser.add_argument("-d405", "--Directory405", help="405 Images Directory")
-    parser.add_argument("-rf", "--RatiosFile", help="Ratios File")
-    parser.add_argument("-wv485", "--WellValues485", help="485 Well Values File")
-    parser.add_argument("-wv405", "--WellValues405", help="405 Well Values File")
+    parser.add_argument("-d485", "--Directory485", help="485 Data Files Directory")
+    parser.add_argument("-d405", "--Directory405", help="405 Data Files Directory")
     parser.add_argument("-gd", "--DirectoryGrav", help="Gravity files directory")
+    parser.add_argument("-dout", "--DirectoryOut", help="Output Directory")
+
 
     args = parser.parse_args()
 
-    print("405 Images Directory {} \n 485 Images Directory {} \n Ratios File {} \n"
-          "405 Well Values File {} \n 485 Well Values File {} \n Gravity Directory {} \n".format(
-        args.Directory405, args.Directory485, args.RatiosFile,
-        args.WellValues405, args.WellValues485, args.DirectoryGrav)
+    print(
+        "405 Data Files Directory {} \n 485 Data Files Directory {} \n"
+         "Gravity Directory {} \n Output Directory {} \n"
+        .format(
+            args.Directory405, args.Directory485, args.RatiosFile,
+            args.WellValues405, args.WellValues485, args.DirectoryGrav,
+            args.DirectoryOut
+        )
     )
 
     return args
 
 
-#######################################################
-# read405Files: read in 405 text files
-# flips each row to match the well orders with the 485 images.
-# returns: list of well samples at time t,
-# each row contains 96 well values.
-#
-# file format is well#:wellValue
-#
-def read405Files(files405_list):
+
+def read405Files(dir405):
+    """
+
+    :param dir405:
+    :rtype : list
+    """
     print('Reading 405 files...'),
+    files405_list = [f for f in os.listdir(dir405)]
     values405_list = []
     count = 0
     for f in files405_list:
-        thisfile = open(basedir405 + f)
+        thisfile = open(dir405 + f)
         lines = thisfile.readlines()
         thisfile.close()
         values405_list.append([])
@@ -49,48 +51,56 @@ def read405Files(files405_list):
             val = int(strs[1].strip())
             values405_list[count].append(val)
 
+        #reverse to match the 485 well order
         values405_list[count].reverse()
         count += 1
 
     print('{}'.format(count))
     return values405_list
 
-#######################################################
-# read485Files: read in 485 text files
-# returns: list of well samples at time t, each row
-# contains 96 well values.
-#
-# file format is well#:wellValue
-#
-def read485Files(files485_list):
+
+def read485Files(dir485):
+    """
+
+
+    :rtype : list
+    :param dir485:
+    :return:
+    """
     print('Reading 485 files...'),
+    files485_list = [f for f in os.listdir(dir485)]
     values485_list = []
     count = 0
     for f in files485_list:
-        thisfile = open(basedir485 + f)
+        thisfile = open(dir485 + f)
         lines = thisfile.readlines()
         thisfile.close()
         values485_list.append([])
+
         for s in lines:
             strs = s.split(':')
             val = int(strs[1].strip())
             values485_list[count].append(val)
+
         count += 1
 
     print('{}'.format(count))
     return values485_list
 
-#######################################################
-#  readGravityFiles: read in gravity text files
-#
-#  returns: a list of gravity vector magnitudes for every
-#  sample in the filesGravity_list
-#
-def readGravityFiles(filesGravity_list):
+
+def readGravityFiles(gravDir):
+    """
+
+
+    :rtype : list
+    :param gravDir:
+    :return:
+    """
     print('Reading and calculating gravity vectors...'),
+    gravityFiles = [f for f in os.listdir(gravDir)]
     gravity_list = []
-    for f in filesGravity_list:
-        thisfile = open(gravitydir + f)
+    for f in gravityFiles:
+        thisfile = open(gravDir + f)
         line = thisfile.readlines()[0]
         thisfile.close()
 
@@ -101,17 +111,20 @@ def readGravityFiles(filesGravity_list):
     print('{}'.format(len(gravity_list)))
     return gravity_list
 
-#################################################################################
-#  calculateRatios: compute the ratios of 405/485
-#
-#  returns: a list of "columns" where each column is actually just another list
-#  Each column represents the values at time t for that well value.
-#
-def calculateRatios(smaller, values405_list, values485_list):
+
+def calculateRatios(smaller, values405, values485):
+    """
+
+    :rtype : list
+    :param smaller:
+    :param values405:
+    :param values485:
+    :return:
+    """
     ratios_list = []
     for sampNum in range(smaller):
-        samples405 = values405_list[sampNum]
-        samples485 = values485_list[sampNum]
+        samples405 = values405[sampNum]
+        samples485 = values485[sampNum]
         if not (len(samples405) == 96 and len(samples485) == 96):
             print('dadnamit, not both have 96 wells en im!')
             exit()
@@ -121,97 +134,130 @@ def calculateRatios(smaller, values405_list, values485_list):
             ratios_list[sampNum].append(r)
     return ratios_list
 
-#######################################################
-#  write405Values
-#
-def write405Values(values405_filename, values405_list, gravity_list):
-    print('Writing 405 values to {0}'.format(values405_filename))
-    values405_file = open(values405_filename, 'w')
-    for i in range(len(values405_list)):
-        row = values405_list[i]
-        values405_file.write("{0} ".format(str(i)))
-        values405_file.write("{0} ".format(str(gravity_list[i])))
-        values405_file.write(' '.join(str(x) for x in values405_list[i]))
-        values405_file.write('\n')
-    values405_file.close()
 
-#######################################################
-#  write485Values
-#
-def write485Values(values485_filename, values485_list, gravity_list):
-    print('Writing 485 values to {0}'.format(values485_filename))
-    values485_file = open(values485_filename, 'w')
-    for i in range(len(values485_list)):
-        values485_file.write("{0} ".format(str(i)))
-        values485_file.write("{0} ".format(str(gravity_list[i])))
-        values485_file.write(' '.join(str(x) for x in values485_list[i]))
-        values485_file.write('\n')
-    values485_file.close()
+def calculateRatioAveragesPerWell(ratios):
+    """
 
-#######################################################
-#  writeRatioValues
-#
-def writeRatioValues(ratios_filename, ratios_list, gravity_list, ratioAvgs_list):
-    print('Writing ratio values to {0}'.format(ratios_filename))
-    ratios_file = open(ratios_filename, 'w')
-    for i in range(len(ratios_list)):
-        ratios_file.write("{0} ".format(str(i)))
-        if gravity_list != None:
-            ratios_file.write("{0} ".format(str(gravity_list[i])))
-        ratios_file.write(' '.join(str(x) for x in ratios_list[i]))
-        ratios_file.write('\n')
-    ratios_file.write(' '.join(str(x) for x in ratioAvgs_list))
-    ratios_file.close()
-
-#######################################################
-#  ratioAveragesPerWell
-#  returns: a list of the averages of every well over
-#  all the samples in ratios_list
-#
-def ratioAveragesPerWell(ratios_list):
+    :rtype : list
+    :param ratios: list of ratios
+    :return: list of ratio averages across all time
+    """
     ratioAvgs_list = []
-    for wellIdx in range(len(ratios_list)):
+    for wellIdx in range(len(ratios)):
         wellSum = 0
-        for smpl in range(len(ratios_list[wellIdx])):
-            wellSum = wellSum + ratios_list[wellIdx][smpl]
+        for smpl in range(len(ratios[wellIdx])):
+            wellSum = wellSum + ratios[wellIdx][smpl]
 
-        wellSum = wellSum / len(ratios_list[wellIdx])
+        wellSum = wellSum / len(ratios[wellIdx])
         ratioAvgs_list.append(wellSum)
     return ratioAvgs_list
+
+def calculateConcentrations(ratios, val405, val485):
+    """
+
+    :rtype : list
+    :param ratios: the well ratios on t=[0..tmax]
+    :param val405: 405 well values on t=[0..tmax]
+    :param val485: 485 well values on t=[0..tmax]
+    """
+    egtaWells = [72,73,74,75]
+    ionoWells = [60,61,62,63]
+    Kd = 0.23
+    Rmin = min( )
+    concs = []
+
+
+def writeValues(fileName, valuesList):
+    """
+    Write a list of values to fileName
+    :param fileName: file to write to
+    :param valuesList: the values to write.
+    """
+    print('Writing values: {0}'.format(fileName))
+    f = open(fileName, 'w')
+    for i in range(len(valuesList)):
+        f.write('{0} '.format(str(i)))
+        f.write(' '.join(str(x) for x in valuesList[i]))
+        f.write('\n')
+    f.close()
 
 
 if __name__ == '__main__':
     args = getArgs()
     basedir485 = args.Directory485
     basedir405 = args.Directory405
-    gravitydir = args.DirectoryGrav
-    values485_filename = args.WellValues485
-    values405_filename = args.WellValues405
-    ratios_filename = args.RatiosFile
+    gravDir = args.DirectoryGrav
 
-    filesGravity_list = [f for f in os.listdir(gravitydir)]
-    files485_list = [f for f in os.listdir(basedir485)]
-    files405_list = [f for f in os.listdir(basedir405)]
+    wv485Name = 'wv485.dat'
+    wv405Name = 'wv405.dat'
+    ratName = 'rat.dat'
+    gravName = 'grav.dat'
 
-    gravity_list = readGravityFiles(filesGravity_list)
-    values405_list = read405Files(files405_list)
-    values485_list = read485Files(files485_list)
+    #list of 96-element lists
+    gravities = readGravityFiles(gravDir)
+    values405 = read405Files(basedir405)
+    values485 = read485Files(basedir485)
 
-    if len(files485_list) < len(files405_list):
-        smaller = len(files485_list)
-        if len(filesGravity_list) < smaller:
-            smaller = len(filesGravity_list)
+    if len(values485) < len(values405):
+        smaller = len(values485)
+        if len(gravities) < smaller:
+            smaller = len(gravities)
     else:
-        smaller = len(files405_list)
-        if len(filesGravity_list) < smaller:
-            smaller = len(filesGravity_list)
+        smaller = len(values405)
+        if len(gravities) < smaller:
+            smaller = len(gravities)
 
-    ratios_list = calculateRatios(smaller, values405_list, values485_list)
-    ratioAvgs_list = ratioAveragesPerWell(ratios_list)
+    ratios = calculateRatios(smaller, values405, values485)
 
-    write405Values(values405_filename, values405_list, gravity_list)
-    write485Values(values485_filename, values485_list, gravity_list)
-    writeRatioValues(ratios_filename, ratios_list, gravity_list, ratioAvgs_list)
+    concs = calculateConcentrations(ratios, values405, values485)
+
+    writeValues(wv405Name, values405)
+    writeValues(wv485Name, values485)
+    writeValues(ratName, ratios)
+    writeValues(gravName, gravities)
 
 
 
+
+
+
+    #######################################################
+    #  write405Values
+    #
+    # def write405Values(values405_filename, values405_list, gravity_list):
+    #     print('Writing 405 values to {0}'.format(values405_filename))
+    #     values405_file = open(values405_filename, 'w')
+    #     for i in range(len(values405_list)):
+    #         row = values405_list[i]
+    #         values405_file.write("{0} ".format(str(i)))
+    #         values405_file.write("{0} ".format(str(gravity_list[i])))
+    #         values405_file.write(' '.join(str(x) for x in values405_list[i]))
+    #         values405_file.write('\n')
+    #     values405_file.close()
+
+    #######################################################
+    #  write485Values
+    #
+    # def write485Values(values485_filename, values485_list, gravity_list):
+    #     print('Writing 485 values to {0}'.format(values485_filename))
+    #     values485_file = open(values485_filename, 'w')
+    #     for i in range(len(values485_list)):
+    #         values485_file.write("{0} ".format(str(i)))
+    #         values485_file.write("{0} ".format(str(gravity_list[i])))
+    #         values485_file.write(' '.join(str(x) for x in values485_list[i]))
+    #         values485_file.write('\n')
+    #     values485_file.close()
+
+    #######################################################
+    #  writeRatioValues
+    #
+    # def writeRatioValues(ratios_filename, ratios_list, gravity_list):
+    #     print('Writing ratio values to {0}'.format(ratios_filename))
+    #     ratios_file = open(ratios_filename, 'w')
+    #     for i in range(len(ratios_list)):
+    #         ratios_file.write("{0} ".format(str(i)))
+    #         if gravity_list != None:
+    #             ratios_file.write("{0} ".format(str(gravity_list[i])))
+    #         ratios_file.write(' '.join(str(x) for x in ratios_list[i]))
+    #         ratios_file.write('\n')
+    #     ratios_file.close()
