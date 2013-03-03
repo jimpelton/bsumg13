7939b18890c11fd94ff7c6594e2bf58a26d58e00
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-03-03 07:34:36 +0000

    Added CaptureTest project.

---
+++ flightsoft/trunk/CaptureTest/BufferPoolTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaptureTest
+{
+    [TestClass]
+    public class BufferPoolTest
+    {
+        [TestMethod]
+        public void PeekFullCopy()
+        {
+        }
+
+        [TestMethod]
+        public void PopEmpty()
+        {
+        }
+
+    //    [TestMethod]
+    //    public void 
+    }
+}
---
+++ flightsoft/trunk/CaptureTest/BufferTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaptureTest
+{
+    [TestClass]
+    public class BufferTest
+    {
+        [TestMethod]
+        public void BufferTest()
+        {
+        }
+ 
+    }
+}
