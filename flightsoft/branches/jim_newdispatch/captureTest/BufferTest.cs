11434649f03a981ea03b3511b983d356c59b6e7d
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-04-18 02:41:27 +0000

    re-added captureTest folder.

---
+++ flightsoft/branches/jim_newdispatch/captureTest/BufferTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using uGCapture;
+namespace CaptureTest
+{
+    [TestClass]
+    public class BufferTest
+    {
+        [TestMethod]
+        public void BufferTest1()
+        {
+            Buffer<byte> buff = new Buffer<byte>(10);
+            byte[] b = new byte[10];
+            Random r = new Random();
+            r.NextBytes(b);
+            buff.setData(b, BufferType.USHORT_IMAGE405);
+            Assert.IsTrue(b.Length == (int)buff.CapacityUtilization);
+        }
+
+        [TestMethod]
+        public void BufferTest_SetDataTest()
+        {
+            Buffer<byte> buff = new Buffer<byte>(10);
+            byte[] b = new byte[10];
+            new Random().NextBytes(b);
+            buff.setData(b, BufferType.USHORT_IMAGE405);
+            
+            byte[] actual = buff.Data;
+            Assert.IsTrue(BufferType.USHORT_IMAGE405 == buff.Type);
+            Assert.IsTrue(b.Length == actual.Length);
+            CollectionAssert.AreEqual(b, actual);
+        }
+ 
+        [TestMethod]
+        public void BufferTest_CopyConstructorTest()
+        {
+            Buffer<byte> buff = new Buffer<byte>(10);
+
+            byte[] b = new byte[10];
+            new Random().NextBytes(b);
+            buff.setData(b, BufferType.USHORT_IMAGE405);
+
+            Buffer<byte> buffcopy = new Buffer<byte>(buff);
+            Assert.AreEqual(buff.Type, buffcopy.Type);                     //type
+            Assert.AreEqual(buff.Text, buffcopy.Text);                     //text
+            Assert.AreEqual(buff.Length, buffcopy.Length);                 //length
+            CollectionAssert.AreEqual(buff.Data, buffcopy.Data);           //data contents
+        }
+    }
+}
