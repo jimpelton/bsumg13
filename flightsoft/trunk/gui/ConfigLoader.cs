9a6f67f39b9f75374e3baed6d8d971b7cee7464f
256games@gmail.com <256games@gmail.com@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-04-28 18:53:25 +0000

    Created a config loader in the gui and a config file (comma seperated)

---
+++ flightsoft/trunk/gui/ConfigLoader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace gui
+{
+    class ConfigLoader
+    {
+
+        public ConfigLoader()
+        {
+
+        }
+
+        public static ConfigData LoadConfig(string configpath)
+        {
+            ConfigData config = new ConfigData();
+            StreamReader reader = new StreamReader(configpath);
+            string Line = "";
+            try
+            {
+                do
+                {
+                    Line = reader.ReadLine();
+                    string[] tokens = Line.Split(',');
+                    if(tokens.Length>1&&tokens[0].Equals("Path"))
+                        config.Path = tokens[1];
+                }
+                while (reader.Peek() != -1);
+            }
+
+            catch
+            {
+                
+            }
+
+            finally
+            {
+                reader.Close();
+            }
+             return config;
+        }
+       
+    }
+
+    public class ConfigData
+    {
+        public string Path
+        {
+           get;
+           set;
+        }
+    }
+}
