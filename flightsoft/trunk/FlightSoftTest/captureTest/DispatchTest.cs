3f6c26fb8cb83249969d9aa9f06ed4fb0511270f
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-05-05 15:23:13 +0000

    Added FlightSoftTest solution and project along with NUnit test libraries.

---
+++ flightsoft/trunk/FlightSoftTest/captureTest/DispatchTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using uGCapture;
+
+namespace captureTest
+{
+    [TestFixture]
+    public class DispatchTest
+    {
+        internal class TinyMockMessage : Message
+        {
+            public TinyMockMessage(Receiver sender) : base(sender)
+            {
+            }
+
+            public override void execute(Receiver r)
+            {
+                r.exTest(r, this);
+            }
+        }
+
+        internal class TinyMockReceiver : Receiver
+        {
+            
+            public TinyMockReceiver(string id, bool receiving = true) 
+                : base(id, receiving)
+            {
+            }
+
+            public void init()
+            {
+                dp.Register(this);
+            }
+
+            private int msgCount = 0;
+            public int MsgCount
+            {
+                get { return msgCount; }
+                set { msgCount = value; }
+            }
+
+            public override void exTest(Receiver r, Message m)
+            {
+                MsgCount++;
+                Console.WriteLine("Received! " + MsgCount);
+            }
+        }
+
+        internal class TinyMockSender : Receiver
+        {
+            public TinyMockSender(string id, bool receiving = true) : base(id, receiving)
+            {
+            }
+            
+            public void SendTinyMockMessage(Receiver from)
+            {
+                dp.Broadcast(new TinyMockMessage(from));
+            }
+
+        }
+
+        [Test]
+        public void SimpleMessageDispatchTest()
+        {
+            TinyMockReceiver tmr = new TinyMockReceiver("tmr");
+            tmr.init();
+            TinyMockSender sender = new TinyMockSender("tinySender");
+            sender.SendTinyMockMessage(sender);
+
+            //give the message time to queue up and be delivered.
+            Thread.Sleep(10);
+            Assert.AreEqual(1, tmr.MsgCount);
+
+            // kick the thread out of the execute loop.
+            tmr.IsReceiving = false;
+            Dispatch.Instance().CleanUpThreads();
+        }
+    }
+}
---
+++ flightsoft/trunk/FlightSoftTest/captureTest/WriterTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using NUnit.Framework;
+using uGCapture;
+
+namespace CaptureTest
+{
+    public class WriterTest
+    {
+        private const int NUM_BUFFERS = 10;
+
+        [Test]
+        public void PhidgetsFileWritingTest1()
+        {
+            
+            BufferPool<Byte> bufpool = new BufferPool<byte>(NUM_BUFFERS, (int)Math.Pow(2, 24));
+            Writer testWriter = new Writer(bufpool, "Writer");
+            testWriter.FrameTime = 500;
+            testWriter.TickerEnabled = true;
+            for (byte i = 0; i < 200; i++)
+            {
+                byte[] bytes = { i++, i++, i++, i++, i++, i++, i++, i++, i++, i++, i++};
+                Buffer<Byte> buf;
+                buf = bufpool.PopEmpty();
+                buf.setData(bytes, BufferType.UTF8_PHIDGETS);
+                buf.Text = "test";
+                testWriter.DirectoryName = "Test";
+                bufpool.PostFull(buf);
+            }
+        }
+
+        private string testdir = @".\TestData\";
+
+        private BufferPool<byte> bufPool;
+        private Writer testWriter;
+        private Thread t;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            bufPool = new BufferPool<byte>(NUM_BUFFERS, (int)Math.Pow(2,24));
+            if (!Directory.Exists(testdir))
+            {
+                Directory.CreateDirectory(testdir);
+            }
+            testWriter = new Writer(bufPool, "TestWriter")
+                {
+                    DirectoryName = testdir
+                };
+            t = new Thread(() => Writer.WriteData(testWriter));
+            t.Start();
+        }
+
+        [Test]
+        public void MockDataWritingTest()
+        {
+            MockController mc = new MockController(bufPool, "MockController");
+            mc.Initialize();
+            mc.ManualPushData();
+        }
+        [TearDown]
+        public void After()
+        {
+            testWriter.IsRunning = false;
+            try
+            {
+                t.Join();
+            }
+            catch (Exception eek)
+            {
+                Console.WriteLine(eek.StackTrace);
+            }
+        }
+    }
+}
