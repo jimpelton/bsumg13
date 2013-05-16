bc2835e28ea4985a6c010ad5b9231451da3d736e
256games@gmail.com <256games@gmail.com@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-05-16 06:21:13 +0000

    Forgot something...

---
+++ flightsoft/trunk/gui/BatteryStatusStrings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gui
+{
+    class BatteryStatusStrings
+    {
+        public static string GetBatteryStatusStr(int m)
+        {
+            switch (m)
+            {
+                case (1):
+                    return "Discharging";
+                case (2):
+                    return "AC Power";
+                case (3):
+                    return "Fully Charged";
+                case (4):
+                    return "Low";
+                case (5):
+                    return "Critical";
+                case (6)://all of these are different charging states                           
+                case (7):
+                case (8):
+                case (9):
+                    return "Charging";
+                //skipped 10 ( undefined )
+                case (11):
+                    return "Part Charged";
+                default:
+                    return "Uhg!";
+            }
+        }
+    }
+}
