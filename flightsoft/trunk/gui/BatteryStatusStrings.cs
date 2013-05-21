137c7a280e61d9657f58041d1e07853da4b4ad7a
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-05-21 16:19:35 +0000

    removed unneeded break's from batterystatusstrings.

--- flightsoft/trunk/gui/BatteryStatusStrings.cs
+++ flightsoft/trunk/gui/BatteryStatusStrings.cs
@@ -14,29 +14,22 @@ namespace gui
             {
                 case (1):
                     return "Discharging";
-                    break;
                 case (2):
                     return "AC Power";
-                    break;
                 case (3):
                     return "Fully Charged";
-                    break;
                 case (4):
                     return "Low";
-                    break;
                 case (5):
                     return "Critical";
-                    break;
                 case (6): goto case (9);//all of these are different charging states                           
                 case (7): goto case (9);
                 case (8): goto case (9);
                 case (9):
                     return "Charging";
-                    break;
                 //skipped 10 ( undefined )
                 case (11):
                     return "Part Charged";
-                    break;
                 default:
                     return null;
             }
