a832adb8b8c4f9d44c2d085ba77c8e80e4a5b16b
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-06-26 15:46:18 +0000

    added dat2excel.py

---
+++ imgproc/trunk/python/dat2excel/dat2excel.py
@@ -0,0 +1,18 @@
+import sys
+
+__author__ = 'jim'
+
+def parse_arguments():
+    import argparse
+    parser = argparse.ArgumentParser(prog='dat2excel',
+                                     description='Convert uG .dat files to .xlsx files',
+                                    )
+
+def main(argv):
+   pass 
+
+
+
+if __name__ == '__main__':
+    main(sys.argv) 
+    
