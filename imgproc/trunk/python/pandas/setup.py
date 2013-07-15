52afeeb467f78f0a8136ad9619b56a3f18b560cd
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@8a743791-3894-73bd-ef63-e145c2a61efa>
2013-07-15 19:25:04 +0000

    changes to setup.py for datetime. needs more work still.

--- imgproc/trunk/python/pandas/setup.py
+++ imgproc/trunk/python/pandas/setup.py
@@ -8,7 +8,7 @@ df485_filepath = '/home/jim/z/microgravity/2013/ug_data/2013_06_04_0840/Data485.
 dfgrav_filepath = '/home/jim/z/microgravity/2013/ug_data/2013_06_04_0840/grav.dat'
 dfphid_filepath = '/home/jim/z/microgravity/2013/ug_data/2013_06_04_0840/phid.dat'
 
-def elaptime(time_str):
+def elaptime_from_hmsf(time_str):
     try:
         et = pd.datetime.strptime(time_str, "%H:%M:%S.%f")
     except ValueError:
@@ -16,6 +16,28 @@ def elaptime(time_str):
     dt = et - pd.datetime.strptime("0:0:0.000000", "%H:%M:%S.%f")
     return dt
 
+def elaptime_from_seconds(time_str):
+    try:
+        a = str(time_str).split('.')
+        seconds = int(a[0])
+        micro = int(a[1])*1000
+    except Error as e:
+        raise e
+    
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    from pandas import datetime
+    from datetime import datetime, timedelta
+    return timedelta(hours=hours, minutes=minutes, seconds=seconds, microseconds=micro)
+
+#    string = '{0}:{1}:{2}.{3}'.format(hours,minutes,seconds,millis)
+#     
+#    et = pd.datetime.strptime(time_str, "%S.%f")
+#        et = pd.datetime.strptime("0", "%S")
+#        print("couldn't do that time! {} Returning time 0".format(time_str))
+#
+#    return et - pd.datetime.strptime("0", "%S")
 
 def groupPlates(string):
     if string.startswith("L"):
@@ -26,42 +48,38 @@ def groupPlates(string):
         return ""
 
 
-def load_df(path_str, sep=' ', index_col=0, parse_dates=True, converters=None):
+def load_df(path_str, 
+            sep=' ', 
+            index_col=0,
+            parse_dates=True,
+            converters=None):
 
-    if not converters: converters = {'Elapsed': elaptime}
+    if not converters: converters = {'Elapsed': elaptime_from_hmsf}
     return pd.read_csv(path_str,
-                       sep=' ',
+                       sep=sep,
                        index_col=index_col,
                        parse_dates=parse_dates,
                        converters=converters)
 
-def truncate(data_frame, times, skipcol=1, cols=None):
-    """
+def load_df_raw(path_str, 
+                sep=' ', 
+                index_col=0, 
+                parse_dates=True,
+                converters=None):
 
-    :param data_frame:
-    :param times:
-    :param skipcol:
-    :param cols:
-    :return:
-    """
-    Gavgs = []
-    Tavgs = []
-    for ts in times:
-        idx = data_frame.index.searchsorted(ts)
-        idxLow = idx-3
-        idxHigh = idx+3
-        if idxLow < 0:
-            idxLow = 0
-        if idxHigh > len(data_frame.index):
-            idxHigh = len(data_frame.index)
+    if not converters: converters = {'Elapsed' : elaptime_from_seconds}
 
-        Gs = data_frame.ix[idxLow:idxHigh,:]
-        Gavg = Gs.ix[:,skipcol:].mean()
-        Tavgs.append(ts)
-        Gavgs.append([data_frame['Elapsed'].ix[idx], Gavg])
+    cols = ['Elapsed']
+    cols = cols.extend(makeWellColumnTitles())
 
-    dti = pd.DatetimeIndex(Tavgs)
-    return pd.DataFrame(data=Gavgs, index=dti, columns=cols)
+    return pd.read_csv(path_str,
+                       sep=sep,
+                       index_col=index_col,
+                       parse_dates=parse_dates,
+                       converters=converters,
+                       header=None,
+                       names=cols)
+                       
 
 def find_closest_times(search, times):
     """
@@ -73,3 +91,67 @@ def find_closest_times(search, times):
     """
     return pd.Series([search.index.asof(dt) for dt in times.index])
 
+def makeWellColumnTitles():
+    alpha = [chr(c) for c in range(65, 73)]  # 'A' through 'H'
+    cols = []
+    lr = ["L", "R"]
+    for i in range(1, 13):
+        for w in range(0, 2):
+            p = lr[w]
+            for c in alpha:
+                cols.append(p + str(c) + str(i))
+    return cols
+
+def makeDateTimeIndex(timeList, name="Time"):
+    dti = pandas.DatetimeIndex(data=timeList, name=name, tz="UTC")
+    dti.name = name
+    return dti
+
+def makeElapsedColumn(valuesDict: dict, name="Elapsed"):
+    dtMin = self._updateStartTime(valuesDict)
+    for frame in valuesDict.values():
+        if frame is None:
+            continue
+        frame.insert(loc=0, column=name,
+                value=frame.index.map(lambda x: x-dtMin))
+
+def findStartTime(valuesDict: dict):
+    from pytz import timezone as tz
+    from pandas import datetime
+
+    dtMin = datetime(2020, 1, 1, tzinfo=tz("UTC"))
+    for frame in valuesDict.values():
+        if frame is None:
+            continue
+        m = frame.index.min()
+        if m < dtMin:
+            dtMin = m
+    return dtMin
+
+# def truncate(data_frame, times, skipcol=1, cols=None):
+#     """
+#
+#     :param data_frame:
+#     :param times:
+#     :param skipcol:
+#     :param cols:
+#     :return:
+#     """
+#     Gavgs = []
+#     Tavgs = []
+#     for ts in times:
+#         idx = data_frame.index.searchsorted(ts)
+#         idxLow = idx-3
+#         idxHigh = idx+3
+#         if idxLow < 0:
+#             idxLow = 0
+#         if idxHigh > len(data_frame.index):
+#             idxHigh = len(data_frame.index)
+#
+#         Gs = data_frame.ix[idxLow:idxHigh,:]
+#         Gavg = Gs.ix[:,skipcol:].mean()
+#         Tavgs.append(ts)
+#         Gavgs.append([data_frame['Elapsed'].ix[idx], Gavg])
+#
+#     dti = pd.DatetimeIndex(Tavgs)
+#     return pd.DataFrame(data=Gavgs, index=dti, columns=cols)
